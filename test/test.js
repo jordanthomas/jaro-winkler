d0d44aa3688dab5fc9113f880a14e9f06a88858f
Jordan Thomas <mail@jordanthomas.me>
2015-02-08 22:27:42 -0500

    Adds some tests.

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var assert   = require('chai').assert;
+var distance = require('../');
+
+describe('distance', function() {
+  it('should return a number', function() {
+    var value = distance('JARO', 'ORAJ');
+    assert.typeOf(value, 'number');
+  });
+
+  it('should return the right distance', function() {
+    // See for basis of expected values:
+    // http://en.wikipedia.org/wiki/Jaro–Winkler_distance
+
+    var value1 = distance('MARTHA', 'MARHTA');
+    assert.closeTo(value1, 0.961, 0.001);
+
+    var value2 = distance('DWAYNE', 'DUANE');
+    assert.closeTo(value2, 0.84, 0.001);
+
+    var value2 = distance('DIXON', 'DICKSONX');
+    assert.closeTo(value2, 0.814, 0.001);
+  });
+});
