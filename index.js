001311a13c8d3881ed7efd3ec6bc39fdf3a575b4
Jordan Thomas <mail@jordanthomas.me>
2015-02-25 00:07:32 -0500

    Cleanup and make whitespace more consistent.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function distance(s1, s2, options) {
   var j;
 
   // Exit early if either are empty.
-  if ( s1.length === 0 || s2.length === 0 ) {
+  if (s1.length === 0 || s2.length === 0) {
     return 0;
   }
 
@@ -29,20 +29,20 @@ function distance(s1, s2, options) {
   }
 
   // Exit early if they're an exact match.
-  if ( s1 === s2 ) {
+  if (s1 === s2) {
     return 1;
   }
 
-  var range     = (Math.floor(Math.max(s1.length, s2.length) / 2)) - 1;
+  var range = (Math.floor(Math.max(s1.length, s2.length) / 2)) - 1;
   var s1Matches = new Array(s1.length);
   var s2Matches = new Array(s2.length);
 
-  for ( i = 0; i < s1.length; i++ ) {
+  for (i = 0; i < s1.length; i++) {
     var low  = (i >= range) ? i - range : 0;
     var high = (i + range <= s2.length) ? (i + range) : (s2.length - 1);
 
-    for ( j = low; j <= high; j++ ) {
-      if ( s1Matches[i] !== true && s2Matches[j] !== true && s1[i] === s2[j] ) {
+    for (j = low; j <= high; j++) {
+      if (s1Matches[i] !== true && s2Matches[j] !== true && s1[i] === s2[j]) {
         ++m;
         s1Matches[i] = s2Matches[j] = true;
         break;
@@ -51,7 +51,7 @@ function distance(s1, s2, options) {
   }
 
   // Exit early if no matches were found.
-  if ( m === 0 ) {
+  if (m === 0) {
     return 0;
   }
 
@@ -59,27 +59,27 @@ function distance(s1, s2, options) {
   var k = 0;
   var numTrans = 0;
 
-  for ( i = 0; i < s1.length; i++ ) {
-    if ( s1Matches[i] === true ) {
-      for ( j = k; j < s2.length; j++ ) {
-        if ( s2Matches[j] === true ) {
+  for (i = 0; i < s1.length; i++) {
+    if (s1Matches[i] === true) {
+      for (j = k; j < s2.length; j++) {
+        if (s2Matches[j] === true) {
           k = j + 1;
           break;
         }
       }
 
-      if ( s1[i] !== s2[j] ) {
+      if (s1[i] !== s2[j]) {
         ++numTrans;
       }
     }
   }
 
   var weight = (m / s1.length + m / s2.length + (m - (numTrans / 2)) / m) / 3;
-  var l      = 0;
-  var p      = 0.1;
+  var l = 0;
+  var p = 0.1;
 
-  if ( weight > 0.7 ) {
-    while ( s1[l] === s2[l] && l < 4 ) {
+  if (weight > 0.7) {
+    while (s1[l] === s2[l] && l < 4) {
       ++l;
     }
 
